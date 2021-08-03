8b758959ad8ea06a9e18b31d5e0b4c21fb235a6a
Soyeong Kim <soyeong.kim.95@gmail.com>
2021-08-03 14:55:04 +0900

    git init
    Signed-off-by: Soyeong Kim <soyeong.kim.95@gmail.com>

---
+++ src/main/java/com/example/webpjt01/WebPjt01Application.java
@@ -0,0 +1,13 @@
+package com.example.webpjt01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebPjt01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebPjt01Application.class, args);
+    }
+
+}
